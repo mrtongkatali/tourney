c27a129418976c0b05fd41265424d1ce1417c33a
Leo Diaz <leoangelo.diaz@gmail.com>
2025-01-05 22:34:36 +0800

    Bootstrapping tournament endpoint

---
+++ api/Controllers/TournamentController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/tournament")]
+    public class Tournament : ControllerBase
+    {
+    }
+}
