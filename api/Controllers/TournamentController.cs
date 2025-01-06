207c2ad019712131d13b916cd042d34afd701cde
Leo Diaz <leoangelo.diaz@gmail.com>
2025-01-07 00:08:34 +0800

    Checkpoint: setting up tournament endpoint

--- api/Controllers/TournamentController.cs
+++ api/Controllers/TournamentController.cs
@@ -6,5 +6,40 @@ namespace tourney.api.Controllers
     [Route("api/tournament")]
     public class Tournament : ControllerBase
     {
+        [HttpGet("{id}")]
+        public IActionResult GetTournamentById(int id)
+        {
+            return Ok($"Get tournament with id {id}");
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Ok("Create a new tournament");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id)
+        {
+            return Ok($"Update tournament with id {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok($"Delete tournament with id {id}");
+        }
+
+        [HttpPost("publish/{id}")]
+        public IActionResult Publish(int id)
+        {
+            return Ok($"Update tournament with id {id}");
+        }
+
+        [HttpGet("{id}/teams")]
+        public IActionResult GetSignedTeams(int id)
+        {
+            return Ok($"Get teams for tournament with id {id}");
+        }
     }
 }
