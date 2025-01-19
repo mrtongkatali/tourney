f3d85dc6b280fc9151ce41e36cbaf4ff65f5914a
Leo Diaz <leoangelo.diaz@gmail.com>
2025-01-19 15:35:34 +0800

    Add Status and Description in TeamsTbl

---
+++ api/Migrations/20250119073430_AddStatusAndDescriptionInTeamsTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusAndDescriptionInTeamsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "teams",
+                type: "varchar(2000)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "teams",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "teams");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "teams");
+        }
+    }
+}
--- api/Models/Team.cs
+++ api/Models/Team.cs
@@ -3,6 +3,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace tourney.api.Models;
 
+public enum TeamStatus
+{
+    Active = 1,
+    Inactive = 2,
+}
+
 [Table("teams")]
 public class Team : BaseEntity
 {
@@ -16,5 +22,11 @@ public class Team : BaseEntity
     [Column("name", TypeName = "varchar(50)")]
     [Required(ErrorMessage = "Team name is required")]
     public string Name { get; set; } = string.Empty;
+
+    [Column("description", TypeName = "varchar(2000)")]
+    public string Description { get; set; } = string.Empty;
+
+    [Column("status")]
+    public TeamStatus Status { get; set; } = TeamStatus.Active;
     public Tournament? Tournament { get; set; }
 }
